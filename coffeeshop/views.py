363227c66ba4f05241b994ef889c9c4837e445b9
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2013-05-16 16:50:18 +0100

    Added another view with a template

--- coffeeshop/views.py
+++ coffeeshop/views.py
@@ -1,6 +1,14 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader, Context
 
 def index(request):
     result = HttpResponse("Greetings Osney One")
     return result
+
+def inducks(request):
+    t = loader.get_template('inducks.html')
+    filler = Context()
+    result = HttpResponse(t.render(filler))
+    return result
+
