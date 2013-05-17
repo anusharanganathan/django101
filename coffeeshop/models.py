a1857b821afc887a00555b53b641dfe2da901153
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2013-05-17 14:05:36 +0100

    Have added a model consisting of a single database table

--- coffeeshop/models.py
+++ coffeeshop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Activity(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    duration = models.IntegerField()
+    description = models.TextField()
+
